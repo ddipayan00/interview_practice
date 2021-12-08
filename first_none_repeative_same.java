6e48d65251642b084c3522418e46b06547406eb0
ddipayan00 <47278363+ddipayan00@users.noreply.github.com>
2021-12-09 00:35:13 +0530

    Create first_none_repeative_same.java

---
+++ first_none_repeative_same.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		// How to find the first non repeated character of a given String?
+		Scanner sc = new Scanner(System.in);
+		String str = "aadwkdi";
+		// Using HashMap
+		HashMap<Character,Integer> mp = new HashMap<Character,Integer>();
+		for(int i=0;i<str.length();i++){
+		    char x = str.charAt(i);
+		    if(mp.containsKey(x)){
+		        int y = mp.get(x);
+		        mp.put(x,y+1);
+		    }
+		    else{
+		        mp.put(x,1);
+		    }
+		}
+		for(int i=0;i<str.length();i++){
+		    char x = str.charAt(i);
+		    if(mp.get(x)==1){
+		        System.out.print("Using HashMap : "+x);
+		        break;
+		    }
+		}
+		// Using Loop
+		for(int i=0;i<str.length();i++){
+		    boolean flag = false;
+		    for(int j=0;j<str.length();j++){
+		        if(str.charAt(i) == str.charAt(j) && i != j ){
+		            flag = true;
+		            break;
+		        }
+		    }
+		    if(flag == false){
+		        System.out.print("\nUsing Loop : "+str.charAt(i));
+		        break;
+		    }
+		}
+	}
+}
